bc3dd5488e199ce652294f86d8f331bdb3f93cee
Anson <ansfoong@gmail.com>
2020-09-10 05:08:47 -0400

    Added Google Maps Feature

--- lib/screens/customer.dart
+++ lib/screens/customer.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CustomerScreen extends StatefulWidget {
   CustomerScreen({ Key key }): super(key: key);
@@ -8,10 +11,38 @@ class CustomerScreen extends StatefulWidget {
 }
 
 class _CustomerScreenState extends State<CustomerScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.7670169511878, -122.42184275),
+    zoom: 64.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.7670169511878, -122.42184275),
+      tilt: 59.440717697143555,
+      zoom: 500);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Customer'),
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: {
+          Marker(
+            markerId: MarkerId(_kGooglePlex.toString()),
+            position: _kGooglePlex.target,
+            infoWindow: InfoWindow(
+              title: 'Test',
+            )
+          )
+        },
+      ),
     );
   }
 }
